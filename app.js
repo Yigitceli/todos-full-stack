961ee06872185d5e1e26cc2d25582efb585d05e5
Yigitceli <Yigit.celi@outlook.com>
2021-09-02 18:32:01 +0300

    cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 //
 
 app.use(morgan("dev"));
-app.use(cors({origin: "http://localhost:3000", credentials:true}));
+app.use(cors({origin: "https://todos-yigit.netlify.app", credentials:true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
